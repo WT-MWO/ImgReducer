3f1b32673815fe40ea0a1f60c23f0cd3f309b3ed
Marcin Woliński <mrcwolinski@gmail.com>
2022-11-11 21:03:30 +0100

    functionalities and comments added

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from imagehandler import ImageHandler
-
+from imagehandler import ImageHandler, FileModifier
 
 #paste you folder path 
 dir = r"C:\Users\m_wol\OneDrive\Pulpit\pics"
 
 imghandler = ImageHandler(dir)
+imgmodifier = FileModifier(dir)
+
 imghandler.get_images()
-#place for modification functions
+
+##place for modification functions
 imghandler.size_reduce()
 
-#saving images, as copy add prefix etc.
+##saving images, as copy add prefix etc.
 imghandler.save_image(as_copy = True)
+
+##run this to rename in order, possibly add date
+imgmodifier.image_rename(name= "IMG", add_date_year=True)

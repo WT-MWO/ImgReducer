4cfa0d8595b86595794fd35242b618f77291c895
Marcin Wolinski <mwo@woodthilsted.com>
2024-03-12 20:42:00 +0100

    Example added.

--- img_to_pdf_example.py
+++ img_to_pdf_example.py
@@ -5,14 +5,12 @@ from src.img_to_pdf import Img_to_pdf
 jpg_folder_path = "C:\\Users\\mwo\\test"
 pdf_name = 'test.pdf'
 
-
-# CODE
+# Reduce size of images
 imghandler = ImageHandler(jpg_folder_path)
 imghandler.get_images()
-# Place for modification functions
 imghandler.size_reduce()
-# Saving images, as copy add prefix etc.
+# Saving reduced images
 imghandler.save_reduced_image(as_copy=False, quality=25)
-
+# Combine images to one pdf file
 img_to_pdf = Img_to_pdf(path=jpg_folder_path, pdf_name=pdf_name)
 img_to_pdf.save()
